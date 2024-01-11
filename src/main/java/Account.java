c5e837fd57ad75bf577b3afde1aa70f602ee8b5c
saralyna.ouyahia.etu <saralyna.ouyahia.etu@a12p10.fil.univ-lille.fr>
2024-01-11 15:33:33 +0100

    code du test, test passé au vert

---
+++ src/main/java/Account.java
@@ -0,0 +1,19 @@
+
+public class Account {
+	int credit;
+	int debit;
+	
+	public Account() {
+		this.credit=0;
+		this.debit=0;
+	}
+	
+	public int getCredit() {
+		return this.credit;
+	}
+	
+	public int getDebit() {
+		return this.debit;
+	}
+
+}
