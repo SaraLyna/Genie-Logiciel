a658cbe8f9c39232ed5312977b21fd99a49555d7
saralyna.ouyahia.etu <saralyna.ouyahia.etu@a12p10.fil.univ-lille.fr>
2024-01-11 16:01:33 +0100

    refactoring

--- src/test/java/AccountSaraTest.java
+++ src/test/java/AccountSaraTest.java
@@ -1,30 +1,38 @@
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
 import static org.junit.Assert.*;  
 
 public class AccountSaraTest {
+	//private Account account;
+	
+	//Refactoring , mais ça ne marche pas
+//	@BeforeEach
+//	public void init() {
+//		account = new Account();
+//	}
 	
 	
 	@Test
-	public void testCompte (){
-		Account myCompte = new Account();
-		assertEquals(0, myCompte.getCredit());
-		assertEquals(0, myCompte.getDebit());
+	public void testCompte(){
+		Account account = new Account();
+		assertEquals(0, account.getCredit());
+		assertEquals(0, account.getDebit());
 		
 	}
 	
 	@Test
 	public void testCreditIncrease() {
-		Account myCompte = new Account();
-		myCompte.credit(20);
-		assertEquals(20, myCompte.getCredit());
+		Account account = new Account();
+		account.credit(20);
+		assertEquals(20, account.getCredit());
 	}
 	
 	@Test
 	public void testDebitIncrease() {
-		Account myCompte = new Account();
-		myCompte.credit(20);
-		myCompte.debit(10);
-		assertEquals(10, myCompte.getDebit());
+		Account account = new Account();
+		account.credit(20);
+		account.debit(10);
+		assertEquals(10, account.getDebit());
 		
 	}
 
