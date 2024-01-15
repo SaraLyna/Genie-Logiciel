39cf1da340d0ed78d066aba397f110a7fe0606d6
saralyna.ouyahia.etu <saralyna.ouyahia.etu@a10p2.fil.univ-lille.fr>
2024-01-15 16:59:56 +0100

    5.1 test rouge

---
+++ src/test/java/BankTest.java
@@ -0,0 +1,15 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class BankTest {
+	
+	 @Test
+	    public void testEmptyBank() {
+	        Bank bank = new Bank();
+
+	        assertTrue(bank.getAccounts().isEmpty());
+	    }
+
+}
