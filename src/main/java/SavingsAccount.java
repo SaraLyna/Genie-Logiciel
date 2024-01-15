988dd84fa982e4df77c4dedbc7b3937a7bdccf26
saralyna.ouyahia.etu <saralyna.ouyahia.etu@a10p2.fil.univ-lille.fr>
2024-01-15 15:48:26 +0100

    test 4.1 vert

--- src/main/java/SavingsAccount.java
+++ src/main/java/SavingsAccount.java
@@ -1,4 +1,10 @@
+import java.util.*;
 
 public class SavingsAccount extends Account {
+    private double interestRate;
 
+    public SavingsAccount(double interestRate) {
+        super(); 
+        this.interestRate = interestRate;
+    }
 }
