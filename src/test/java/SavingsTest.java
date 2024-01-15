32494d1b8026a4c30843cb2b6268966e8253e10a
saralyna.ouyahia.etu <saralyna.ouyahia.etu@a10p2.fil.univ-lille.fr>
2024-01-15 15:52:16 +0100

    test rouge

--- src/test/java/SavingsTest.java
+++ src/test/java/SavingsTest.java
@@ -5,11 +5,21 @@ public class SavingsTest {
 
     @Test
     public void testCreationSavingsAccount() {
-        SavingsAccount savingsAccount = new SavingsAccount(0.02);
+        SavingsAccount savingsAccount = new SavingsAccount(0.02);// taux d'interets à 2%
         
         assertEquals(0, savingsAccount.getCredit());
         assertEquals(0, savingsAccount.getDebit());
         assertEquals(0, savingsAccount.getSold());
+    }
+    
+    @Test
+    public void testCreditAndCalculateSold() {
+        SavingsAccount savingsAccount = new SavingsAccount(0.02); 
+        
+        savingsAccount.credit(1000);
+        assertEquals(1020, savingsAccount.getSold(), 0.01);
+        savingsAccount.calculateSold();
+        assertEquals(1040.4, savingsAccount.getSold(), 0.01);
     }
 
 }
